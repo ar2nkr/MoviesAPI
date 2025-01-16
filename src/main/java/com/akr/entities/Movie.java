8622bb51ce7251ced021adcfdac6b10c03c6eafe
ar2nkr <ar2nkr@gmail.com>
2025-01-16 17:40:31 +0530

    file handling done

---
+++ src/main/java/com/akr/entities/Movie.java
@@ -0,0 +1,47 @@
+package com.akr.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Integer movieId;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Please provide movie's title")
+   private String title;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Please provide movie's director")
+   private String director;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Please provide movie's studio")
+   private String studio;
+
+    @ElementCollection
+    @CollectionTable(name = "movie_cast")
+   private Set<String> movieCast;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Please provide movie's release year")
+   private Integer releaseYear;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Please provide movie's poster")
+   private String poster;
+
+}
---
+++ src/main/java/com/akr/repos/MovieRepo.java
@@ -0,0 +1,9 @@
+package com.akr.repos;
+
+import com.akr.entities.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepo extends JpaRepository<Movie, Integer> {
+}
